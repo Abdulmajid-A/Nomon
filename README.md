28f2167e32f94b9588652d044a52d8885a23c6b0
Abdulmajid Almesned <abdulmajeed1922@gmail.com>
2021-01-10 21:38:29 +0300

    updated vterm checks, auto update packages and README from .md to .org and its content

--- README.md
+++
@@ -1,3 +0,0 @@
-*Nomon* is an emacs configuration repo that aims for ease of use, configuring, performance and understandability, hence,
-it's at continuous updates and modification.
-Any suggestion is welcomed.
--- emacs_config/auto_package_update_init.el
+++ emacs_config/auto_package_update_init.el
@@ -1,8 +1,17 @@
-(use-package auto-package-update
-  :straight t
-  :if window-system
-  :config
-  (setq auto-package-update-prompt-before-update t)
-  (setq auto-package-update-delete-old-versions t)
-  ;;(setq auto-package-update-hide-results t)
-  (auto-package-update-maybe))
+(when (or (not(f-exists? ".locks/auto_package_update_last_check.lk"))
+	  (s-less? (f-read-text ".locks/auto_package_update_last_check.lk") (format-time-string "%Y:%m:%d")))
+  (use-package auto-package-update
+    :straight t
+    :if window-system
+    :config
+    (setq auto-package-update-prompt-before-update t)
+    (setq auto-package-update-delete-old-versions t)
+    (unless (f-exists? ".locks/auto_package_update_last_check.lk")
+      (f-touch ".locks/auto_package_update_last_check.lk")
+      )
+    (f-write-text (format-time-string "%Y:%m:%d") 'utf-8 ".locks/auto_package_update_last_check.lk")
+    ;;(setq auto-package-update-hide-results t)
+    (auto-package-update-maybe)
+    )
+
+  )
